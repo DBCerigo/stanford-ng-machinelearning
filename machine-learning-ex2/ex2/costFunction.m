6d735ecb1a458c17add99046f1a0d96ccfde148d
dbcerigo <dbcerigo@gmail.com>
2016-04-10 09:58:17 +0100

    implment costFunction.m function and gradien t

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,11 +20,14 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+preds = sigmoid(X * theta);
+posErrs = -y' * log(preds);
+negErrs = -(1 - y') * log(1 - preds);
 
+J = (1/m) * sum(posErrs + negErrs);
+% ===
 
-
-
-
+grad = (1/m) * X' * (preds - y);
 
 
 % =============================================================
