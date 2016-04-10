7a1ced3f9e21ae1719a9fd0971ab02c4b1a446cc
dbcerigo <dbcerigo@gmail.com>
2016-04-10 10:13:26 +0100

    add comment to decision boundary plot to explain code

--- machine-learning-ex2/ex2/plotDecisionBoundary.m
+++ machine-learning-ex2/ex2/plotDecisionBoundary.m
@@ -1,10 +1,10 @@
 function plotDecisionBoundary(theta, X, y)
 %PLOTDECISIONBOUNDARY Plots the data points X and y into a new figure with
 %the decision boundary defined by theta
-%   PLOTDECISIONBOUNDARY(theta, X,y) plots the data points with + for the 
-%   positive examples and o for the negative examples. X is assumed to be 
-%   a either 
-%   1) Mx3 matrix, where the first column is an all-ones column for the 
+%   PLOTDECISIONBOUNDARY(theta, X,y) plots the data points with + for the
+%   positive examples and o for the negative examples. X is assumed to be
+%   a either
+%   1) Mx3 matrix, where the first column is an all-ones column for the
 %      intercept.
 %   2) MxN, N>3 matrix, where the first column is all-ones
 
@@ -17,11 +17,12 @@ if size(X, 2) <= 3
     plot_x = [min(X(:,2))-2,  max(X(:,2))+2];
 
     % Calculate the decision boundary line
+    % This is solving for x_2, given theta^T *X = 0
     plot_y = (-1./theta(3)).*(theta(2).*plot_x + theta(1));
 
     % Plot, and adjust axes for better viewing
     plot(plot_x, plot_y)
-    
+
     % Legend, specific for the exercise
     legend('Admitted', 'Not admitted', 'Decision Boundary')
     axis([30, 100, 30, 100])
