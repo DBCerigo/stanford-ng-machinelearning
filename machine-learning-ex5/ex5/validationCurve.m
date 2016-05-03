3966a221f3c20d0598e9cad22c0db73308858778
dbcerigo <dbcerigo@gmail.com>
2016-05-03 14:48:12 +0100

    implement validation curve

--- machine-learning-ex5/ex5/validationCurve.m
+++ machine-learning-ex5/ex5/validationCurve.m
@@ -17,27 +17,30 @@ error_train = zeros(length(lambda_vec), 1);
 error_val = zeros(length(lambda_vec), 1);
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return training errors in 
-%               error_train and the validation errors in error_val. The 
-%               vector lambda_vec contains the different lambda parameters 
-%               to use for each calculation of the errors, i.e, 
-%               error_train(i), and error_val(i) should give 
-%               you the errors obtained after training with 
+% Instructions: Fill in this function to return training errors in
+%               error_train and the validation errors in error_val. The
+%               vector lambda_vec contains the different lambda parameters
+%               to use for each calculation of the errors, i.e,
+%               error_train(i), and error_val(i) should give
+%               you the errors obtained after training with
 %               lambda = lambda_vec(i)
 %
 % Note: You can loop over lambda_vec with the following:
 %
 %       for i = 1:length(lambda_vec)
 %           lambda = lambda_vec(i);
-%           % Compute train / val errors when training linear 
+%           % Compute train / val errors when training linear
 %           % regression with regularization parameter lambda
 %           % You should store the result in error_train(i)
 %           % and error_val(i)
 %           ....
-%           
 %       end
-%
-%
+
+for i = 1:length(lambda_vec)
+  lambda = lambda_vec(i);
+  thetai = trainLinearReg(X, y, lambda);
+  error_train(i) = linearRegCostFunction(X, y, thetai, 0)(1);
+  error_val(i) = linearRegCostFunction(Xval, yval, thetai, 0)(1);
 
 
 
